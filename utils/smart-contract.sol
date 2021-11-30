a35648652ae1574142e1537f9a85183ddeff07e7
vincenzo-dip8 <vincenzopaolo.diperna@outlook.com>
2021-11-30 19:03:41 +0100

    commit

--- utils/smart-contract.sol
+++ utils/smart-contract.sol
@@ -2,38 +2,28 @@
 pragma solidity >=0.4.0 <0.9.0;
 
 contract RestaurantContract{
-    address payable _owner; //"0x684F22798FEf8dDcaCB8278447703787293cEe07"
-    mapping(bytes32 => bytes32) hashTable;
+    address payable _owner; 
+    mapping(string => string) hashTable;
 
     constructor() {
-        _owner = msg.sender;
+        _owner = payable(msg.sender);
     }
+
+    receive () external payable{}
 
     modifier onlyOwner(){
         require(msg.sender == _owner, "Not owner");
         _;
     }
 
-    function forwardPayment(string memory _orderHash) public payable {
-        require(msg.value > 0);
-        _owner.transfer(msg.value);
+    function forwardPayment(uint256 amount, string memory _orderHash) public payable {
+        require(msg.value > amount);
+        (bool sent, ) = _owner.call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
+        hashTable[_orderHash]= _orderHash;
     }
 
-    // function deposit(uint256 amount) payable public {
-    //     require(msg.value == amount);
-    //     address(this).transfer(msg.value);
-    // }
-
-    // function withdraw() payable public onlyOwner {
-    //     // require(msg.sender == _owner);
-    //     msg.sender.transfer(address(this).balance);
-    // }
-
-    function putData(bytes32 _orderHash, bytes32 _hashTransaction) public{
-        hashTable[_orderHash]= _hashTransaction ;
-    }
-
-    function getData(bytes32 _orderHash) view public onlyOwner returns(bytes32) {
+    function getData(string memory _orderHash) view public onlyOwner returns(string memory) {
         // require(msg.sender == _owner);
         return hashTable[_orderHash];
     }
